a26ad8f8b7d4add4893f665ac8a86d711388191a
Jian Hui Lee <jianhui.lee@canonical.com>
2023-07-13 09:35:20 +0800

    GENIO: vcodec: mt8395: venc: fix security issue
    1 venc query cap copy to global variable instead of
    static variable addr send to user space then send back
    2 vpud_task_mutex need to protect all vcud_task usage
    
    CR-Id: AUTO00218250
    
    Signed-off-by: Daoli Yu <daoli.yu@mediatek.com>
    Change-Id: Ia9e14a1d839cab2886050f9a431b902d05ece1ab
    (backported from https://gitlab.com/mediatek/aiot/bsp/mtk-vcodec-driver/-/commit/cd699f74ee70b92cc86338f9faf7dcd0971a49f5)
    Signed-off-by: Jian Hui Lee <jianhui.lee@canonical.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- ubuntu/mtk-vcodec-driver/mt8395/venc/venc_vcu_if.c
+++ ubuntu/mtk-vcodec-driver/mt8395/venc/venc_vcu_if.c
@@ -25,9 +25,9 @@ static void handle_enc_init_msg(struct venc_vcu_inst *vcu, void *data)
 	vcu->vsi = VCU_FPTR(vcu_mapping_dm_addr)(vcu->dev, msg->vcu_inst_addr);
 }
 
-static void handle_query_cap_ack_msg(struct venc_vcu_ipi_query_cap_ack *msg)
+static void handle_query_cap_ack_msg(struct venc_vcu_inst *vcu,
+	struct venc_vcu_ipi_query_cap_ack *msg)
 {
-	struct venc_vcu_inst *vcu = (struct venc_vcu_inst *)msg->ap_inst_addr;
 	void *data;
 	int size = 0;
 
@@ -42,12 +42,12 @@ static void handle_query_cap_ack_msg(struct venc_vcu_ipi_query_cap_ack *msg)
 	switch (msg->id) {
 	case GET_PARAM_VENC_CAP_SUPPORTED_FORMATS:
 		size = sizeof(struct mtk_video_fmt);
-		memcpy((void *)msg->ap_data_addr, data,
+		memcpy((void *)mtk_venc_formats, data,
 			size * MTK_MAX_ENC_CODECS_SUPPORT);
 		break;
 	case GET_PARAM_VENC_CAP_FRAME_SIZES:
 		size = sizeof(struct mtk_codec_framesizes);
-		memcpy((void *)msg->ap_data_addr, data,
+		memcpy((void *)mtk_venc_framesizes, data,
 			size * MTK_MAX_ENC_CODECS_SUPPORT);
 		break;
 	default:
@@ -170,9 +170,11 @@ int vcu_enc_ipi_handler(void *data, unsigned int len, void *priv)
 	if (msg == NULL || task == NULL ||
 	   task->tgid != current->tgid ||
 	   (struct venc_vcu_inst *)(unsigned long)msg->venc_inst == NULL) {
+		VCU_FPTR(vcu_put_task)();
 		ret = -EINVAL;
 		return ret;
 	}
+	VCU_FPTR(vcu_put_task)();
 
 	vcu = (struct venc_vcu_inst *)(unsigned long)msg->venc_inst;
 
@@ -234,7 +236,7 @@ int vcu_enc_ipi_handler(void *data, unsigned int len, void *priv)
 		ret = 1;
 		break;
 	case VCU_IPIMSG_ENC_QUERY_CAP_DONE:
-		handle_query_cap_ack_msg(data);
+		handle_query_cap_ack_msg(vcu, data);
 		break;
 	case VCU_IPIMSG_ENC_WAIT_ISR:
 		if (msg->status == MTK_VENC_CORE_0)
@@ -322,9 +324,11 @@ static int vcu_enc_send_msg(struct venc_vcu_inst *vcu, void *msg,
 		if (task)
 			mtk_vcodec_err(vcu, "send fail pid: inst %d curr %d",
 				vcu->daemon_pid, task->tgid);
+		VCU_FPTR(vcu_put_task)();
 		vcu->abort = 1;
 		return -EIO;
 	}
+	VCU_FPTR(vcu_put_task)();
 
 	status = VCU_FPTR(vcu_ipi_send)(vcu->dev, vcu->id, msg, len, vcu->ctx->dev);
 	if (status) {
@@ -353,6 +357,7 @@ void vcu_enc_set_pid(struct venc_vcu_inst *vcu)
 		vcu->daemon_pid = task->tgid;
 	else
 		vcu->daemon_pid = -1;
+	VCU_FPTR(vcu_put_task)();
 }
 
 int vcu_enc_set_ctx(struct venc_vcu_inst *vcu,
