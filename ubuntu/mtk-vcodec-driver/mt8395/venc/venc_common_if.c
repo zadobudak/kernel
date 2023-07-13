9c33d2b236232fcb6b840a04c45c8410429c4dbd
Jian Hui Lee <jianhui.lee@canonical.com>
2023-07-13 09:19:07 +0800

    GENIO: mtk-vcodec-driver: mt8395: add check for wait_for_done_ctx
    1. add null check for arg of mtk_vcodec_wait_for_done_ctx
    2. add vcu->ctx check at ipi handlers
    3. add missing venc vcu ipi handler ctx list check
    
    CR-Id:AUTO00210309
    
    Signed-off-by: Daoli Yu <daoli.yu@mediatek.com>
    Change-Id: Ibf20abbd59e5b9d24f4fee1ed81164418cf26633
    (backported from
    https://gitlab.com/mediatek/aiot/bsp/mtk-vcodec-driver/-/commit/a03d41454a696c4b1b8d3e4279ab8579816bfccf)
    Signed-off-by: Jian Hui Lee <jianhui.lee@canonical.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- ubuntu/mtk-vcodec-driver/mt8395/venc/venc_common_if.c
+++ ubuntu/mtk-vcodec-driver/mt8395/venc/venc_common_if.c
@@ -228,8 +228,6 @@ static int venc_init(struct mtk_vcodec_ctx *ctx, unsigned long *handle)
 		return -ENOMEM;
 	}
 
-	mtk_vcodec_add_ctx_list(ctx);
-
 	inst->ctx = ctx;
 	inst->vcu_inst.ctx = ctx;
 	inst->vcu_inst.dev = VCU_FPTR(vcu_get_plat_device)(ctx->dev->plat_dev);
@@ -272,8 +270,10 @@ static int venc_init(struct mtk_vcodec_ctx *ctx, unsigned long *handle)
 	}
 	inst->hw_base = mtk_vcodec_get_enc_reg_addr(inst->ctx, VENC_SYS);
 	inst->vcu_inst.handler = vcu_enc_ipi_handler;
+	(*handle) = (unsigned long)inst;
 
 	mtk_vcodec_debug_enter(inst);
+	mtk_vcodec_add_ctx_list(ctx);
 
 	ret = vcu_enc_init(&inst->vcu_inst);
 
@@ -295,8 +295,7 @@ static int venc_init(struct mtk_vcodec_ctx *ctx, unsigned long *handle)
 		mtk_vcodec_del_ctx_list(ctx);
 		kfree(inst);
 		(*handle) = (unsigned long)NULL;
-	} else
-		(*handle) = (unsigned long)inst;
+	}
 
 	return ret;
 }
