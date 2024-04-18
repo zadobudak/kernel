0ac7a52b664e8fff424611748257582d76649251
Ken Wang <ken@radxa.com>
2024-04-18 10:18:51 +0800

    driver: hwmon: emc2305: delete duplicate definition
    This structure has been defined in linux/platform_data/emc2305.h.
    
    Signed-off-by: Ken Wang <ken@radxa.com>

--- drivers/hwmon/emc2305.c
+++ drivers/hwmon/emc2305.c
@@ -97,20 +97,6 @@ struct emc2305_cdev_data {
 };
 
 /**
- * struct emc2305_platform_data - EMC2305 driver platform data
- * @max_state: maximum cooling state of the cooling device;
- * @pwm_num: number of active channels;
- * @pwm_separate: separate PWM settings for every channel;
- * @pwm_min: array of minimum PWM per channel;
- */
-struct emc2305_platform_data {
-	u8 max_state;
-	u8 pwm_num;
-	bool pwm_separate;
-	u8 pwm_min[EMC2305_PWM_MAX];
-};
-
-/**
  * @client: i2c client;
  * @hwmon_dev: hwmon device;
  * @max_state: maximum cooling state of the cooling device;
